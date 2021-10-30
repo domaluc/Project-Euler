c00cda5e90806bfff27b00cb8b0cfb913781f461
domlucchesi <55263760+domlucchesi@users.noreply.github.com>
2021-10-30 14:07:54 -0700

    Create even_fibonacci_numbers.rs

---
+++ 002/even_fibonacci_numbers.rs
@@ -0,0 +1,17 @@
+fn fibonacci_even(limit: u32) -> u32 {
+    let mut even_sum = 0u32;
+    let mut x;
+    let mut y = 1u32;
+    let mut z = 2u32;
+    while even_sum < limit {
+        even_sum += z;
+        x = y + z;
+        y = x + z;
+        z = x + y;
+    }
+    even_sum
+}
+
+fn main() {
+    println!("{}", fibonacci_even(4_000_000));
+}
